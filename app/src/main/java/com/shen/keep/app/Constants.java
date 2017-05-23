4bcff646acfa463f9ca059717fa6995cdd3da14c
shenjianli <cqtddt@163.com>
2017-05-23 17:32:34 +0800

    加入开始提示对话框  引入网络请求库

---
+++ app/src/main/java/com/shen/keep/app/Constants.java
@@ -0,0 +1,15 @@
+package com.shen.keep.app;
+
+/**
+ * Created by shenjianli on 2016/7/14.
+ */
+public class Constants {
+
+   public static final String DEV_MODE = "dev";
+
+   public static final String RELEASE_MODE = "release";
+
+   public static final String TEST_MODE = "test";
+
+   public static final int CIRCL_RADIUS = 500;
+}
